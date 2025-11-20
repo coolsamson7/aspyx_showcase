36d68d38a3750a7b00be4ba004e5e444b3fc98d9
Andreas Ernst <andreas.ernst7@gmail.com>
2025-11-20 09:06:52 +0100

    added weather stuff

--- packages/aspyx_message_server/src/aspyx_message_server/component.py
+++ packages/aspyx_message_server/src/aspyx_message_server/component.py
@@ -1,15 +1,15 @@
 from abc import abstractmethod
-from dataclasses import dataclass
 
 from aspyx_service import component, Component, get, Service
+
+from .weather.weather_manager import Weather
+
 
 class  WeatherService(Service):
     @abstractmethod
     @get("weather")
     def get_weather(self) -> Weather:
         pass
-
-
 
 @component(services =[
     WeatherService,
